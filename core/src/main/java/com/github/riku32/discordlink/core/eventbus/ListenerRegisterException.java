e64bd90652f60497cc9d7d178e3ff84b66472c3c
Riku32 <riku@kawaii.sh>
2021-08-06 05:57:06 -0500

    Platform agnostic eventbus and event system

---
+++ core/src/main/java/com/github/riku32/discordlink/core/eventbus/ListenerRegisterException.java
@@ -0,0 +1,7 @@
+package com.github.riku32.discordlink.core.eventbus;
+
+public class ListenerRegisterException extends Exception {
+    public ListenerRegisterException(String message) {
+        super(message);
+    }
+}
