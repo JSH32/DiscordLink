e4b4e0602687f9bc1078108fa66949475f3b30ed
Riku32 <riku@kawaii.sh>
2021-08-06 06:05:14 -0500

    Platform specific plugin

---
+++ core/src/main/java/com/github/riku32/discordlink/core/DiscordLink.java
@@ -0,0 +1,63 @@
+package com.github.riku32.discordlink.core;
+
+import com.github.riku32.discordlink.core.database.Database;
+import com.github.riku32.discordlink.core.platform.PlatformPlugin;
+import lombok.Getter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class DiscordLink {
+    @Getter
+    private final PlatformPlugin plugin;
+
+    @Getter
+    private Database database;
+
+    @Getter
+    private Config config;
+
+    public DiscordLink(PlatformPlugin plugin) {
+        this.plugin = plugin;
+
+        File configFile = new File(plugin.getDataDirectory(), "config.yml");
+        if (!configFile.exists()) {
+            try {
+                Files.copy(Path.of(Objects.requireNonNull(getClass().getResource("config.yml")).getPath()), configFile.toPath());
+                plugin.getLogger().severe("Created a new configuration file, please fill in the file");
+            } catch (IOException e) {
+                plugin.getLogger().severe("Unable to create configuration file");
+                plugin.getLogger().severe(e.getMessage());
+            }
+            disable();
+            return;
+        }
+
+        try {
+            this.config = new Config(new String(Files.readAllBytes(configFile.toPath())));
+        } catch (NoSuchElementException | IOException e) {
+            plugin.getLogger().severe(e.getMessage());
+            disable();
+            return;
+        }
+
+        try {
+            database = new Database(plugin.getDataDirectory());
+        } catch (SQLException e) {
+            plugin.getLogger().severe("Unable to create/start the database");
+            plugin.getLogger().severe(e.getMessage());
+            disable();
+            return;
+        }
+    }
+
+    public void disable() {
+        database.close();
+        plugin.disable();
+    }
+}
