aba4acf0b848599f0459bf25247d4ac14837cb72
Riku32 <riku@kawaii.sh>
2021-08-11 04:14:35 -0500

    Test discord/minecraft interaction

--- core/src/main/java/com/github/riku32/discordlink/core/events/JoinEvent.java
+++ core/src/main/java/com/github/riku32/discordlink/core/events/JoinEvent.java
@@ -5,8 +5,14 @@ import com.github.riku32.discordlink.core.eventbus.events.PlayerJoinEvent;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 public class JoinEvent {
+    private TextChannel channel;
+
+    public JoinEvent(TextChannel channel) {
+        this.channel = channel;
+    }
+
     @EventHandler
     private void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage("Welcome");
+        channel.sendMessage(event.getPlayer().getName() + " joined the server").queue();
     }
 }
